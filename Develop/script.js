1a0a9591eddd766e53e0f8c0a99bdc62e176ab20
GitBen <dowonblee@gmail.com>
2022-05-14 19:44:41 -0700

    finished most of the JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,122 +1,146 @@
-// Assignment code here
-var confirmNumber;
-var confirmCharacter;
-var confirmUpperCase;
-var confirmLowerCase;
-
-character = ["!", "@", "#", "$", "%", "^", "&", "*", ];
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-space =[];
-
-var select;
-//uppercase letters
-var caps = function (x) {
-  return x.toUpperCase();
-};
-
-// variable for uppercase option
-alphacaps = alpha.map(toUpperCase);
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// variables for options
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
 
-  passwordText.value = password;
-// Beginning of prompts
-  select = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-
-  if (!select) {
-    alert ("This is a required step!");
-  } else if (select < 8 || select > 128) {
-    select = parseInt(prompt("You can only have between 8 and 128 characters."));
-  } else {
-    // the 4 different options
-    confirmNumber = confirm("Include Numbers?");
-   
-    confirmCharacter = confirm("Include Special Characters?");
-    
-    confirmUpperCase = confirm("Include UPPERCASE Letters?");
-   
-    confirmLowerCase = confirm("Include lowercase Letters?");
-  };
-  
-  //Beginning of outputs depending on user settings
-
-  // if no options
-  if (!confirmLowerCase && !confirmCharacter && !confirmNumber && !confirmUpperCase) {
-    select = alert ("Please choose a(n) option(s)!");
-  } 
-  
-  // all options
-  else if (confirmLowerCase && confirmCharacter && confirmNumber && confirmUpperCase) {
-    select = character.concat(number, alpha, alphacaps);
-  }
-
-  // selection of 3 options
-  else if (confirmCharacter && confirmLowerCase && confirmUpperCase) {
-    select = character.concat(alpha, alphacaps);
-  }
-  
-  else if (confirmCharacter && confirmNumber && confirmUppercase) {
-    select = character.concat(number, alphacaps);
-  }
-  
-  else if (confirmCharacter && confirmNumber && confirmLowercase) {
-    select = character.concat(number, alpha);
-  }
-  
-  else if (confirmCharacter && confirmLowercase && confirmUppercase) {
-    select = character.concat(alpha, alphacaps);  
-  }
-  //  selection of 2 options
-  else if (confirmLowercase && confirmNumber) {
-    select = alpha.concat(number);
-  } 
-
-  else if (confirmLowercase && confirmUppercase) {
-    select = alpha.concat(alphacaps);
-  } 
- 
-  else if (confirmNumber && confirmUppercase) {
-    select = number.concat(alphacaps);
-  }
-  
-  else if (confirmCharacter && confirmNumber) {
-    select = character.concat(number);
-  }
-  
-  else if (confirmCharacter && confirmLowercase) {
-    select = character.concat(alpha);
-  } 
-  
-  else if (confirmCharacter && confirmUppercase) {
-    select = character.concat(alphacaps);
-  }
- 
-  //  selection of 1 option
-  else if (confirmCharacter) {
-    select = character;
-  }
-
-  else if (confirmLowerCase) {
-    select = alpha;
-  }
-
-  else if (confirmNumber) {
-    select = number;
-  }
-
-  else if (confirmUpperCase) {
-    select = space.concat(alphacaps);
-  }
-
-  
+// Special 
+character = ["!", "#", "$", "%", "&", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "@", " ^ ", "_", "~"];
+// Numbers
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Letters
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Space, not sure what this is for but I saw this from a reference
+space = [];
+// Choices declared outside the if statement so they can be concatenated upon condition
+var select;
+// converts letters to uppercase 
+var toUpper = function (x) {
+    return x.toUpperCase();
 };
+// Uppercase stuff
+alphaCaps = alpha.map(toUpper);
+
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
+
+// The function for password making
+function generatePassword() {
+    // Input prompt
+    enter = parseInt(prompt("Please choose your password to have between 8 and 128 characters"));
+
+    if (!enter) {
+        alert("Please choose a valid number");
+    } 
+    
+    else if (enter < 8 || enter > 128) {
+        enter = parseInt(prompt("You must choose between 8 and 128"));
+
+    } 
+    
+    else {
+        confirmNumber = confirm("Include numbers?");
+        confirmCharacter = confirm("Include special characters?");
+        confirmUppercase = confirm("Include UPPERCASE letters?");
+        confirmLowercase = confirm("Include lowercase letters?");
+    };
+
+    // If no options
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        select = alert("Please select at least one option!");
+    }
+
+    // If all options
+    else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+        select = character.concat(number, alpha, alphaCaps);
+    }
+    
+    // 3 options
+    else if (confirmCharacter && confirmNumber && confirmUppercase) {
+        select = character.concat(number, alphaCaps);
+    }
+    
+    else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        select = character.concat(number, alpha);
+    }
+    
+    else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+        select = character.concat(alpha, alphaCaps);
+    }
+    
+    else if (confirmNumber && confirmLowercase && confirmUppercase) {
+        select = number.concat(alpha, alphaCaps);
+    }
+    
+    // 2 options
+    else if (confirmCharacter && confirmNumber) {
+        select = character.concat(number);
+
+    } 
+    
+    else if (confirmCharacter && confirmLowercase) {
+        select = character.concat(alpha);
+
+    } 
+    
+    else if (confirmCharacter && confirmUppercase) {
+        select = character.concat(alphaCaps);
+    }
+    
+    else if (confirmLowercase && confirmNumber) {
+        select = alpha.concat(number);
+
+    } 
+    
+    else if (confirmLowercase && confirmUppercase) {
+        select = alpha.concat(alphaCaps);
+
+    } 
+    
+    else if (confirmNumber && confirmUppercase) {
+        select = number.concat(alphaCaps);
+    }
+    // Only 1 options
+    else if (confirmCharacter) {
+        select = character;
+    }
+    
+    else if (confirmNumber) {
+        select = number;
+    }
+    
+    else if (confirmLowercase) {
+        select = alpha;
+    }
+    
+    else if (confirmUppercase) {
+        select = space.concat(alphaCaps);
+    };
+
+    var password = [];
+
+  //  code for random variable selection
+    for (var i = 0; i < enter; i++) {
+        var pickChoices = select[Math.floor(Math.random() * select.length)];
+        password.push(pickChoices);
+    }
+  
+    // combines password stuff into a string
+    var ps = password.join("");
+    UserInput(ps);
+    return ps;
+}
+
+// puts password into textbox
+function UserInput(ps) {
+    document.getElementById("password").textContent = ps;
+}
